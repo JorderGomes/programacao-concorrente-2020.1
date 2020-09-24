60c69b0d7b7ca7f2fb9b588aa843ddedf1a739a7
JorderGomes <jordergomes@hotmail.com>
2020-09-24 13:32:57 -0300

    inicio estudos lua

---
+++ LUA/ex1.lua
@@ -0,0 +1,5 @@
+print("result = " .. 2+3);
+
+a = 33 * 7;
+
+print("a multiplicação é " .. a);
---
+++ LUA/ex2.lua
@@ -0,0 +1,36 @@
+variavel = 2
+print(variavel)
+
+variavel = "agua"
+print(variavel)
+
+-- nil -- not int list: é o null das outras linguagens
+
+--table : tabela ->
+pessoa = {
+    nome = "Eric",
+    idade = 26
+}
+
+print(pessoa.nome)
+
+-- função
+function soma(a,b)
+    return a + b
+end
+
+print(soma(1,2))
+
+
+s = soma
+
+print(s(1,3))
+
+--thread corotinas
+-- userdata : tipos definidos por usuarios
+
+
+
+
+
+
